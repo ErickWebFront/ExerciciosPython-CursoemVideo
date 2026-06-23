d21f176902021d99ce8eac37886e9e7876d5679a
erickxavier <xaviererickdeoliveiraxavier@gmail.com>
2026-06-22 21:31:58 -0300

    Create operadores.aritimeticos.py

---
+++ operadores.aritimeticos.py
@@ -0,0 +1,7 @@
+6+7 #adição                                orderm de precedencia
+6-7 #subtração                              1: ()
+6*7 #multiplicação                          2: **
+6/7 #divisão                                3: *, /, //, %
+6**7 #exponeciação                          4: +, -
+6//7 #divisão inteira
+6%7 #resto da divisão
